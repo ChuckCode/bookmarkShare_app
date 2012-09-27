ba9cedabe0699abccec8a5bf71a2e5751558bb6a
Chuck <kostlevy@gmail.com>
2012-09-26 19:46:28 -0500

    Before tests

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,5 @@
 class Bookmark < ActiveRecord::Base
   attr_accessible :date_saved, :name, :url
+  validates :url, :presence => true, :length => { :minimum => 10 }
+  validates :name, :presence => true, :length => { :maximum => 100 }
 end
