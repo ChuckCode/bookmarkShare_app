1b4a6033b992b98f6e3f74e367360834d492fc46
Chuck <kostlevy@gmail.com>
2012-10-03 18:02:41 -0500

    Added test database

--- spec/requests/bookmarks_spec.rb
+++ spec/requests/bookmarks_spec.rb
@@ -8,4 +8,11 @@ describe "Bookmarks" do
       visit '/bookmarks'
     end
   end
+  
+  describe "new Bookmark page" do
+      
+    it "should exist" do
+      visit '/bookmarks/new'
+    end
+  end
 end
